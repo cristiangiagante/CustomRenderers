185f475f20124fddf3cc6c4988e469a10e188bb7
Cristian Adrian Giagante <cristiangiagante@hotmail.com>
2017-08-27 17:20:48 -0300

    Resize disabled, add contact

--- Tools/AddCustomFonts/AddCustomFonts/Form1.Designer.cs
+++ Tools/AddCustomFonts/AddCustomFonts/Form1.Designer.cs
@@ -185,9 +185,11 @@
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Add Custom Fonts to Android, iOS and UWP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Custom Fonts to Android, iOS and UWP :: cristiangiagante@hotmail.com";
             this.ResumeLayout(false);
             this.PerformLayout();
 
